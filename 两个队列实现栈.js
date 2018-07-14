35d3fb08ff9eb4c3d8b57185a64d77cf5beea576
sllva <1349777045@qq.com>
2018-07-14 16:13:21 +0800

    两个队列实现栈

---
+++ 两个队列实现栈.js
@@ -0,0 +1,38 @@
+//两个队列实现栈
+function stack(){
+	var queue1=[];
+	var queue2=[];
+	var r;
+	this.addElem=function(elem){
+		if(queue1.length){
+			queue1.push(elem);
+			console.log(queue1);
+		}else{
+			queue2.push(elem);
+			console.log(queue2);
+		}
+	}
+	this.deleteElem=function(){
+		if(queue1.length){
+			queue2=queue1.splice(0,queue1.length-1);
+			r=queue1.pop();
+			console.log(r);
+			return r;
+		}else{
+			queue1=queue2.splice(0,queue2.length-1);
+			r=queue2.pop();
+			console.log(r);
+			return r;
+		}
+	}
+}
+var stack1=new stack();
+stack1.addElem(1);
+stack1.addElem(2);
+stack1.addElem(3);
+stack1.deleteElem();
+stack1.deleteElem();
+stack1.addElem(4);
+stack1.addElem(5);
+stack1.deleteElem();
+
